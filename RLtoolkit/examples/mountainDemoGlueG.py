a092222b797806a7d899046ea06489d2a6d5e2c2
nyasui <nikolaus.yasui@mail.mcgill.ca>
2018-08-16 17:57:20 -0600

    rewrote mountain car demo for rl_glue

--- RLtoolkit/examples/mountainDemoGlueG.py
+++ RLtoolkit/examples/mountainDemoGlueG.py
@@ -14,9 +14,9 @@ import numpy as np
 
 from RLtoolkit.Quickgraph.graph3d import *
 from RLtoolkit.glue_guiwindow import SimulationWindow
+from RLtoolkit.gridworld.gwGlueAgent import QlambdaGridAgent
+from RLtoolkit.tiles3 import IHT, tiles
 
-
-# Graphical display routines
 
 class MCWindow(SimulationWindow):
     def __init__(self, update, wwidth=340, wheight=660, **kwargs):
@@ -102,10 +102,11 @@ class MCWindow(SimulationWindow):
         self.draw_side()
 
     def draw_side(self):
-        pos = 0
+        pos = self.environment.pos
+        h = self.car_height(pos)
+
         # erase previous disk and arrow
         gDelete(self.mcarSideView, self.oldside)
-        h = self.car_height(pos)
         self.oldside = list()
         self.oldside.append(gDrawDisk(self.mcarSideView, pos, h, 0.065,
                                       'white'))
@@ -145,6 +146,15 @@ class Visualizer:
         self.g3d_graph.gDrawView()
         gdSetViewportR(self.g3d_graph, 350, 20, 400, 400)
 
+        self.mc_rl_start = self.mc_window.rl_start
+        self.mc_window.rl_start = self.rl_start
+
+    def rl_start(self):
+        """Rewrite the rl_start function to call the graph update before
+        each episode."""
+        self.g3d_update()
+        self.mc_rl_start()
+
     def g3d_update(self):
         for i in range(self.g3d_graph.res):
             pos = -1.2 + ((0.6 - -1.2) * (float(i) / (self.g3d_graph.res - 1)))
@@ -157,3 +167,73 @@ class Visualizer:
                     d = self.mc_window.agent.statevalue(phi)
                 self.g3d_graph.data[i][j] = d
         self.g3d_graph.gDrawView()
+
+
+class MCEnvironment:
+    def __init__(self, num_tiles=np.array([2, 2]), num_tilings=32):
+        self.vel = None
+        self.pos = None
+        self.actions = [-1, 0, 1]
+
+        self.num_tilings = num_tilings
+        self.size = (num_tiles + 1).prod() * self.num_tilings
+        self.scale = num_tiles / np.array([0.14, 1.7])
+        self.iht = None
+
+    def env_init(self):
+        self.iht = IHT(self.size)
+
+    def get_phi(self, state):
+        floats = ((np.asarray(state) + [0.07, 1.2]) * self.scale).tolist()
+        return tiles(self.iht, self.num_tilings, floats)
+
+    def env_start(self):
+        self.vel = 0
+        self.pos = np.random.uniform(-0.6, -0.4)
+        state = self.vel, self.pos
+
+        return self.get_phi(state)
+
+    def env_step(self, action):
+        self.vel += 0.001 * self.actions[action] - 0.0025 * np.cos(3 * self.pos)
+        self.vel = min(max(-0.07, self.vel), 0.07)
+        self.pos += self.vel
+        self.pos = min(max(-1.2, self.pos), 0.5)
+
+        if self.pos == -1.2:
+            self.vel = 0
+
+        state = self.vel, self.pos
+
+        terminal = self.pos == 0.5
+        reward = -1
+
+        return reward, self.get_phi(state), terminal
+
+    def numactions(self):
+        return len(self.actions)
+
+    def numstates(self):
+        # number of cells
+        return self.size
+
+
+def rundemo():
+    s = Visualizer(30)
+    env = MCEnvironment()
+    s.mc_window.environment = env
+    s.mc_window.agent = QlambdaGridAgent(numactions=env.numactions(),
+                                         numstates=env.numstates(),
+                                         epsilon=0.01,
+                                         alpha=0.5 / env.num_tilings,
+                                         agentlambda=0.9,
+                                         gamma=1)
+    s.mc_window.rl_init()
+    s.mc_window.environment.vel = 0
+    s.mc_window.environment.pos = -0.5
+    s.mc_window.whole_view()
+    gMainLoop()
+
+
+if __name__ == '__main__':
+    rundemo()
