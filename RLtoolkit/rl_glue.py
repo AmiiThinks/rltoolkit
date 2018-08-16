c0db6add2e7dd1b4fc9154644e47ba0422eab45c
nyasui <nikolaus.yasui@mail.mcgill.ca>
2018-08-16 17:57:04 -0600

    cleaned up RL Glue code for readability

--- RLtoolkit/rl_glue.py
+++ RLtoolkit/rl_glue.py
@@ -7,30 +7,40 @@ from abc import ABCMeta, abstractmethod
 
 
 class RLGlue:
-    """RLGlue class.
+    """Facilitates interaction between an agent and environment for
+    reinforcement learning experiments.
 
     args:
         env_obj: an object that implements BaseEnvironment
-        agent_obj: an object that implements AgentEnvironment
+        agent_obj: an object that implements BaseAgent
     """
 
     def __init__(self, env_obj, agent_obj):
         self.environment = env_obj
         self.agent = agent_obj
 
+        # useful statistics
         self.total_reward = None
-        self.last_action = None
         self.num_steps = None
         self.num_episodes = None
         self.num_ep_steps = None
 
+        # the most recent state and action taken by the agent
+        self.last_action = None
+        self.last_state = None
+
     def rl_init(self):
+        # reset statistics
         self.total_reward = 0
-        self.last_action = 0
         self.num_steps = 0
         self.num_episodes = 0
         self.num_ep_steps = 0
 
+        # reset last state and action
+        self.last_action = None
+        self.last_state = None
+
+        # reset agent and environment
         self.agent.agent_init()
         self.environment.env_init()
 
@@ -42,27 +52,47 @@ class RLGlue:
         """
         self.num_ep_steps = 0
 
-        last_state = self.environment.env_start()
-        self.last_action = self.agent.agent_start(last_state)
+        self.last_state = self.environment.env_start()
+        self.last_action = self.agent.agent_start(self.last_state)
 
-        observation = (last_state, self.last_action)
+        return self.last_state, self.last_action
 
-        return observation
+    def rl_step(self):
+        """Takes a step in the RLGlue experiment.
 
+        Returns:
+            (float, state, action, Boolean): reward, last state observation,
+                last action, boolean indicating termination
+        """
+        a = self.last_action
+        reward, self.last_state, terminal = self.environment.env_step(a)
+
+        self.total_reward += reward
+
+        if terminal:
+            self.num_episodes += 1
+            self.agent.agent_end(reward)
+            self.last_action = None
+        else:
+            self.last_action = self.agent.agent_step(reward, self.last_state)
+
+        self.num_ep_steps += 1
+        self.num_steps += 1
+
+        return reward, self.last_state, self.last_action, terminal
+
+    ### CONVENIENCE FUNCTIONS BELOW ###
     def rl_env_start(self):
         """Useful when manually specifying agent actions (for debugging). Starts
         RL-Glue environment.
 
         Returns:
-            (float, state, Boolean): reward, state observation, boolean
-                indicating termination
+            state observation
         """
-        self.total_reward = 0.0
-        self.num_ep_steps = 1
+        self.num_ep_steps = 0
+        self.last_state = self.environment.env_start()
 
-        this_observation = self.environment.env_start()
-
-        return this_observation
+        return self.last_state
 
     def rl_env_step(self, action):
         """Useful when manually specifying agent actions (for debugging).Takes a
@@ -75,10 +105,9 @@ class RLGlue:
             (float, state, Boolean): reward, state observation, boolean
                 indicating termination.
         """
-        ro = self.environment.env_step(action)
-        (this_reward, _, terminal) = ro
+        reward, self.last_state, terminal = self.environment.env_step(action)
 
-        self.total_reward += this_reward
+        self.total_reward += reward
 
         if terminal:
             self.num_episodes += 1
@@ -87,30 +116,7 @@ class RLGlue:
 
         self.num_steps += 1
 
-        return ro
-
-    def rl_step(self):
-        """Takes a step in the RLGlue experiment.
-
-        Returns:
-            (float, state, action, Boolean): reward, last state observation,
-                last action, boolean indicating termination
-        """
-        (reward, state, term) = self.environment.env_step(self.last_action)
-
-        self.total_reward += reward
-
-        if term:
-            self.num_episodes += 1
-            self.agent.agent_end(reward)
-            roat = (reward, state, None, term)
-        else:
-            self.last_action = self.agent.agent_step(reward, state)
-            roat = (reward, state, self.last_action, term)
-
-        self.num_ep_steps += 1
-        self.num_steps += 1
-        return roat
+        return reward, self.last_state, terminal
 
     def rl_episode(self, max_steps_this_episode):
         """Convenience function to run an episode.
@@ -120,19 +126,14 @@ class RLGlue:
                 A value of 0 will result in the episode running until
                 completion.
 
-        Returns:
-            Boolean: if the episode should terminate
         """
-        is_terminal = False
+        terminal = False
 
         self.rl_start()
 
-        while (not is_terminal) and ((max_steps_this_episode == 0) or
-                                     (self.num_steps < max_steps_this_episode)):
-            rl_step_result = self.rl_step()
-            is_terminal = rl_step_result[3]
-
-        return is_terminal
+        while not terminal and ((max_steps_this_episode == 0) or
+                                (self.num_ep_steps < max_steps_this_episode)):
+            _, _, _, terminal = self.rl_step()
 
 
 class BaseAgent:
@@ -153,24 +154,22 @@ class BaseAgent:
         """Initialize agent variables."""
 
     @abstractmethod
-    def agent_start(self, observation):
+    def agent_start(self, state):
         """The first method called when the experiment starts, called after
         the environment starts.
         Args:
-            observation (Numpy array): the state observation from the
-                environment's evn_start function.
+            state (state observation): The agent's current state
+
         Returns:
             The first action the agent takes.
         """
 
     @abstractmethod
-    def agent_step(self, reward, observation):
+    def agent_step(self, reward, state):
         """A step taken by the agent.
         Args:
             reward (float): the reward received for taking the last action taken
-            observation (Numpy array): the state observation from the
-                environment's step based, where the agent ended up after the
-                last step
+            state (state observation): The agent's current state
         Returns:
             The action the agent is taking.
         """
@@ -196,10 +195,6 @@ class BaseEnvironment:
     @abstractmethod
     def __init__(self):
         """Declare environment variables."""
-        reward = None
-        observation = None
-        termination = None
-        self.reward_obs_term = (reward, observation, termination)
 
     @abstractmethod
     def env_init(self):
